46af7829981e1ad8d7e8489b9dc8927963491eca
Minami Ueda <minami.ueda@gmail.com>
2025-04-11 20:04:54 -0400

    Add plotting

--- src/tailestim/__init__.py
+++ src/tailestim/__init__.py
@@ -2,20 +2,24 @@
 #
 # SPDX-License-Identifier: MIT
 
+from .datasets import TailData
+
 from .estimators.base import BaseTailEstimator
 from .estimators.hill import HillEstimator
 from .estimators.moments import MomentsEstimator
 from .estimators.kernel import KernelTypeEstimator
 from .estimators.pickands import PickandsEstimator
 from .estimators.smooth_hill import SmoothHillEstimator
-from .datasets import TailData
+
+from .estimators.estimator_set import TailEstimatorSet
 
 __all__ = [
+    'TailData',
     'BaseTailEstimator',
     'HillEstimator',
     'MomentsEstimator',
     'KernelTypeEstimator',
     'PickandsEstimator',
     'SmoothHillEstimator',
-    'TailData',
+    'TailEstimatorSet',
 ]
